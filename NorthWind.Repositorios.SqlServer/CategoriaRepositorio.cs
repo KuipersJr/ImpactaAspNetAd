335d0b2af2df324610b3daa493034bad207b6809
Vítor Avelino <avelino.vitor@gmail.com>
2017-02-07 23:58:13 -0200

    Commit inicial.

---
+++ NorthWind.Repositorios.SqlServer/CategoriaRepositorio.cs
@@ -0,0 +1,37 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace NorthWind.Repositorios.SqlServer
+{
+    public class CategoriaRepositorio
+    {
+        public DataTable Obter()
+        {
+            var dataTable = new DataTable();
+
+            using (var conexao = new SqlConnection(ConfigurationManager.ConnectionStrings["oficinaConnectionString"].ConnectionString))
+            {
+                conexao.Open();
+
+                const string instrucao = @"SELECT [Id]
+                      ,[Nome]
+                      ,[Email]
+                      ,[DataNascimento]
+                      ,Tipo
+                  FROM [Oficina].[dbo].[Cliente]
+                  Where Nome like '%' + @nome + '%'";
+
+                using (var comando = new SqlCommand(instrucao, conexao))
+                {
+                    using (var dataAdapter = new SqlDataAdapter(comando))
+                    {
+                        dataAdapter.Fill(dataTable);
+                    }
+                }
+            }
+
+            return dataTable;
+        }
+    }
+}
---
+++ NorthWind.WebForms/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="NorthWind.WebForms.Global" Language="C#" %>
