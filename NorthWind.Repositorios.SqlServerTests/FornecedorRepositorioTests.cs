305cb32edf8ea2a54b4823603158654e40e06fca
vitor.avelino <vitor.avelino@concretesolutions.com.br>
2017-02-09 18:12:59 -0200

    Adicionado teste unitório para o repositório Fornecedor.

---
+++ NorthWind.Repositorios.SqlServerTests/FornecedorRepositorioTests.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace NorthWind.Repositorios.SqlServer.Tests
+{
+    [TestClass()]
+    public class FornecedorRepositorioTests
+    {
+        [TestMethod()]
+        public void ObterTest()
+        {
+            var fornecedores = new FornecedorRepositorio().Obter();
+
+            Assert.AreNotEqual(fornecedores.Rows.Count, 0);
+        }
+    }
+}
