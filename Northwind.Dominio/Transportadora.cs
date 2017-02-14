9b9e7d944e56485c2e10aa9c5d28bfc9813530c0
vitor.avelino <vitor.avelino@concretesolutions.com.br>
2017-02-14 16:29:07 -0200

    Adicionada view para criar Transportadora, com validação.

--- Northwind.Dominio/Transportadora.cs
+++ Northwind.Dominio/Transportadora.cs
@@ -1,9 +1,17 @@
-﻿namespace Northwind.Dominio
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Northwind.Dominio
 {
     public class Transportadora
     {
         public int Id { get; set; }
+
+        [Required()]
+        [MinLength(3, ErrorMessage = "O Nome deve ter no mínimo 3 caracteres.")]
         public string Nome { get; set; }
+
+        [Required()]
+        [RegularExpression(@"^\([1-9]{2}\) [2-9][0-9]{3}\-[0-9]{4}$", ErrorMessage = "Digite o Telefone no formato (99) 9999-9999")]
         public string Telefone { get; set; }
     }
 }
