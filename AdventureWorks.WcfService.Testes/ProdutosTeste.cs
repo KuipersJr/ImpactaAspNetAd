0b06f2af1cda6886599c737edb5642aebf4f17f1
vitor.avelino <vitor.avelino@concretesolutions.com.br>
2017-03-06 11:01:32 -0300

    Cliente para o WCF da Adventure Works.

---
+++ AdventureWorks.WcfService.Testes/ProdutosTeste.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AdventureWorks.WcfService.Testes.AdventureWorksServiceReference;
+using System.Linq;
+
+namespace AdventureWorks.WcfService.Testes
+{
+    [TestClass]
+    public class ProdutosTeste
+    {
+        [TestMethod]
+        public void GetTeste()
+        {
+            using (var produtosClient = new ProdutosClient())
+            {
+                var produto = produtosClient.Get(4);
+
+                Assert.IsTrue(produto.Name == "Headset Ball Bearings");
+            }
+        }
+
+        [TestMethod]
+        public void GetByNameTeste()
+        {
+            using (var produtosClient = new ProdutosClient())
+            {
+                var produtos = produtosClient.GetByName("Mountain");
+
+                Assert.IsTrue(produtos.Count() == 94);
+            }
+        }
+    }
+}
