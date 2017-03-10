584a80359e90689782b6027267b22466a11bc12a
Vítor Avelino <avelino.vitor@gmail.com>
2017-03-09 22:46:15 -0300

    Impressão da view de Produto.

--- Northwind.Mvc.EF/ViewModels/ProdutoViewModel.cs
+++ Northwind.Mvc.EF/ViewModels/ProdutoViewModel.cs
@@ -5,21 +5,13 @@ namespace Northwind.Mvc.EF.ViewModels
 {
     public class ProdutoViewModel
     {
-        public ProdutoViewModel()
-        {
-
-        }
-
         public ProdutoViewModel(List<Categoria> categorias, Produto produto = null)
         {
             Produto = produto ?? new Produto();
             Categorias  = categorias;
-            CategoriaSelecionadaId = produto != null ? produto.Categoria.Id : (int?)null;
         }
 
         public Produto Produto { get; set; }
-
-        public int? CategoriaSelecionadaId { get; set; }
         public List<Categoria> Categorias { get; set; }
     }
 }
